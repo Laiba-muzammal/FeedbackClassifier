8978f56e481a75662465aca041601533fd69831e
Laiba Muzammal <155936083+Laiba-muzammal@users.noreply.github.com>
2025-07-16 23:57:50 +0500

    Create comments_data.py

---
+++ comments_data.py
@@ -0,0 +1,17 @@
+comments = [
+    'Amazing product! Absolutely love it.',
+    'Not a good experience. Very disappointing.',
+    'The service was okay, nothing special.',
+    'I loved it! Will definitely recommend to my friends.',
+    'Worst product ever! Waste of money.',
+    'Very bad quality, will never buy again.',
+    'Good quality and affordable. Happy with the purchase.',
+    'Neutral feelings about it, neither good nor bad.',
+    'Excellent quality and very affordable, highly recommend.',
+    'The product broke after a week of use, very poor.',
+    'Best purchase I have made this year!',
+    'Mediocre product, could be better.',
+    'The product exceeded my expectations. Very happy!',
+    'Not bad, but could be improved.',
+    'I don’t like it. Not worth the price.'
+]
